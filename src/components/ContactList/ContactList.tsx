e40e414c5a8a6a16fa6b1691de27029591ded9ec
Ilya Sidorchik <ilya@sidorchik.ru>
2019-11-26 02:06:26 +0300

    Add no-data info

--- src/components/ContactList/ContactList.tsx
+++ src/components/ContactList/ContactList.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import Contact from './Contact';
+import './ContactList.scss';
 
 interface IContact {
 	name: string;
@@ -9,20 +10,26 @@ interface IContact {
 const ContactList: React.FC = () => {
 	const [contacts, setContacts] = useState<Array<IContact> | null>(null);
 
-	useEffect(() => {
-		setContacts([
-			{ name: 'Иван Иванов' },
-			{ name: 'Иван Иванов' },
-			{ name: 'Иван Иванов' },
-			{ name: 'Иван Иванов' }
-		]);
-	}, []);
+	// useEffect(() => {
+	// 	setContacts([
+	// 		{ name: 'Иван Иванов' },
+	// 		{ name: 'Иван Иванов' },
+	// 		{ name: 'Иван Иванов' },
+	// 		{ name: 'Иван Иванов' }
+	// 	]);
+	// }, []);
 
 	return (
-		<>
+		<div className="ContactList">
 			{contacts &&
 				contacts.map(({ name }, i) => <Contact name={name} key={i} />)}
-		</>
+
+			{contacts === null && (
+				<div className="ContactList-Caption">
+					Здесь будут ваши контакты
+				</div>
+			)}
+		</div>
 	);
 };
 
