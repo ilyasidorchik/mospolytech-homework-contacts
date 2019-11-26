ac9aded151490a603ce41fd66f48883b78cd7df1
Ilya Sidorchik <ilya@sidorchik.ru>
2019-11-26 14:12:01 +0300

    Make ContactAdd working

--- src/components/ContactAdd/ContactAdd.tsx
+++ src/components/ContactAdd/ContactAdd.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState, useCallback, ChangeEvent } from 'react';
+import { Redirect } from 'react-router-dom';
+
+import Title from '../Title';
+import { getContactList, addContact } from '../../utils/contacts';
+import './ContactAdd.scss';
 
 const ContactAdd: React.FC = () => {
-	return <div>Contact Adding</div>;
+	const [contactAdded, isContactAdded] = useState<boolean>(false);
+	const [value, setValue] = useState<string>('');
+
+	const handleInputChange = useCallback(
+		(e: ChangeEvent<HTMLInputElement>) => {
+			const { value } = e.target;
+			setValue(value);
+		},
+		[setValue]
+	);
+
+	const handleSubmit = useCallback(
+		(e: ChangeEvent<HTMLFormElement>) => {
+			e.preventDefault();
+
+			addContact(value);
+
+			isContactAdded(true);
+		},
+		[value]
+	);
+
+	return contactAdded ? (
+		<Redirect to={`/contact/${getContactList().length}`} />
+	) : (
+		<div className="ContactAdd">
+			<Title className="ContactAdd-Title">New Contact</Title>
+			<form className="ContactAdd-Form" onSubmit={handleSubmit}>
+				<input
+					className="ContactAdd-Input"
+					type="text"
+                    placeholder="First Name"
+                    autoFocus
+					value={value}
+					onChange={handleInputChange}
+				/>
+				<button className="ContactAdd-Button">Add contact</button>
+			</form>
+		</div>
+	);
 };
 
 export default ContactAdd;
--- src/components/ContactList/ContactList.tsx
+++ src/components/ContactList/ContactList.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import Contact from './Contact';
-import { getContactList, addContact } from '../../utils/contacts';
+import { getContactList } from '../../utils/contacts';
 import Title from '../Title';
 import './ContactList.scss';
 
