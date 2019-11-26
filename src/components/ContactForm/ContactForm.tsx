e289a6de10a7b12fc1c1eea166228f1a60045d62
Ilya Sidorchik <ilya@sidorchik.ru>
2019-11-26 18:35:01 +0300

    Fix ContactForm encapsulation

---
+++ src/components/ContactForm/ContactForm.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useCallback, ChangeEvent } from 'react';
+import { Redirect } from 'react-router-dom';
+
+import Title from '../Title';
+import './ContactForm.scss';
+
+type IContactForm = {
+	id: number;
+	businessFunc: (name: any) => void;
+};
+
+const ContactForm: React.FC<IContactForm> = ({ id, businessFunc }) => {
+	const [contactProcess, isContactProccessed] = useState<boolean>(false);
+	const [value, setValue] = useState<string>('');
+
+	const handleInputChange = useCallback(
+		(e: ChangeEvent<HTMLInputElement>) => {
+			const { value } = e.target;
+			setValue(value);
+		},
+		[setValue]
+	);
+
+	const handleSubmit = useCallback(
+		(e: ChangeEvent<HTMLFormElement>) => {
+			e.preventDefault();
+
+			businessFunc(value);
+
+			isContactProccessed(true);
+		},
+		[businessFunc, value]
+	);
+
+	return contactProcess ? (
+		<Redirect to={`/contact/${id}`} />
+	) : (
+		<div className="ContactForm">
+			<Title className="ContactForm-Title">New Contact</Title>
+			<form className="ContactForm-Form" onSubmit={handleSubmit}>
+				<input
+					className="ContactForm-Input"
+					type="text"
+					placeholder="First Name"
+					autoFocus
+					value={value}
+					onChange={handleInputChange}
+				/>
+				<button className="ContactForm-Button">Add contact</button>
+			</form>
+		</div>
+	);
+};
+
+export default ContactForm;
